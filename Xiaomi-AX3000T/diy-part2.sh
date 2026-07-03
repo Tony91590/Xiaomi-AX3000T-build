4730534ce74c507795adace52b632e9b23e312a5
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-03 17:30:48 +0200

    Update diy-part2.sh

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -6,9 +6,6 @@
 # See /LICENSE for more information.
 # 
 # Custom for Xiaomi AX3000T
-
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 sed -i '/\["FR"\]/s/{ 1, 2 }/{ 1, 1 }/' package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
 sed -i 's/default-settings-chn/default-settings/g' include/target.mk
-grep -n "OpenWrt" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 grep -R '\["FR"\]' package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
