aae3f35666ca0ceb96ff91fd536a1f5492c4db94
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-04 23:11:10 +0200

    Update diy-part2.sh

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -24,5 +24,15 @@ sed -i 's,https://immortalwrt.org/,https://openwrt.org/,g' include/version.mk
 sed -i 's,https://github.com/immortalwrt/immortalwrt/issues,https://bugs.openwrt.org/,g' include/version.mk
 sed -i 's,https://github.com/immortalwrt/immortalwrt/discussions,https://forum.openwrt.org/,g' include/version.mk
 sed -i 's/Xiaomi Mi Router AX3000T (stock layout)/Xiaomi Mi Router AX3000T/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-xiaomi-mi-router-ax3000t-stock.dts
+cat > package/base-files/files/etc/banner << EOF
+  _______                     ________        __
+ |       |.-----.-----.-----.|  |  |  |.----.|  |_
+ |   -   ||  _  |  -__|     ||  |  |  ||   _||   _|
+ |_______||   __|_____|__|__||________||__|  |____|
+          |__| W I R E L E S S   F R E E D O M
+ -----------------------------------------------------
+ %D %V, %C
+ -----------------------------------------------------
+EOF
 grep -n "OpenWrt" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 grep -R '\["FR"\]' package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
