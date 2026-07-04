c60d8a3c6ee807d0dbbc60c5d69a22cc944e581a
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-05 01:31:13 +0200

    Enhance Xiaomi AX3000T customization script
    This script customizes the Xiaomi AX3000T by modifying package dependencies, adding the Argon theme, and configuring WLAN settings.

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -9,5 +9,38 @@
 
 sed -i 's/uboot-envtools/uboot-envtools apk-mbedtls base-files ca-bundle dnsmasq dropbear firewall4 fitblk fstools kmod-crypto-hw-safexcel kmod-gpio-button-hotplug kmod-leds-gpio kmod-nft-offload libc libgcc libustream-mbedtls logd mtd netifd nftables odhcp6c odhcpd-ipv6only ppp ppp-mod-pppoe procd-ujail uboot-envtools uci uclient-fetch urandom-seed urngd wpad-basic-mbedtls kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware luci/g' target/linux/mediatek/filogic/target.mk
 sed -i '/AUTOLOAD:=$(call AutoProbe,mt7915e)/a \  MODPARAMS.mt7915e:=wed_enable=Y' package/kernel/mt76/Makefile
+
+# ===== Argon Theme =====
+
+rm -rf package/luci-theme-argon
+rm -rf package/luci-app-argon-config
+
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+mkdir -p files/etc/uci-defaults
+
+cat > files/etc/uci-defaults/99-default-settings << 'EOF'
+#!/bin/sh
+
+# Configure WLAN
+uci set wireless.@wifi-device[0].disabled='0'
+uci set wireless.@wifi-iface[0].disabled='0'
+uci set wireless.@wifi-iface[0].encryption='none'
+uci set wireless.@wifi-iface[0].ssid="OpenWrt_2.4G"
+uci set wireless.@wifi-device[1].disabled='0'
+uci set wireless.@wifi-iface[1].disabled='0'
+uci set wireless.@wifi-iface[1].encryption='none'
+uci set wireless.@wifi-iface[1].ssid="OpenWrt_5G"
+uci commit wireless
+  
+# Remove this uci-defaults script after first boot execution
+rm -f /etc/uci-defaults/99-default-settings
+
+exit 0
+EOF
+
+chmod +x files/etc/uci-defaults/99-default-settings
+
 cat target/linux/mediatek/filogic/target.mk
 cat package/kernel/mt76/Makefile
