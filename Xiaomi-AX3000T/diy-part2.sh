54627778d4671d648d14246f0ee0ff194f3593ec
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-04 23:16:14 +0200

    Fix turboacc config setting in diy-part2.sh

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -8,6 +8,6 @@
 # Custom for Xiaomi AX3000T
 sed -i '/\["FR"\]/s/{ 1, 2 }/{ 1, 1 }/' package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
 sed -i 's/default-settings-chn/default-settings/g' include/target.mk
-sed 's/"turboacc\.config\.fullcone"="2"/"turboacc.config.fullcone"="0"/g' package/mtk/applications/luci-app-turboacc-mtk/root/etc/uci-defaults/turboacc
+sed -i 's/"turboacc\.config\.fullcone"="2"/"turboacc.config.fullcone"="0"/g' package/mtk/applications/luci-app-turboacc-mtk/root/etc/uci-defaults/turboacc
 cat package/mtk/applications/luci-app-turboacc-mtk/root/etc/uci-defaults/turboacc
 grep -R '\["FR"\]' package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
