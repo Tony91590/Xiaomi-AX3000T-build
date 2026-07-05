4a962a8d02386a8f4ab8040cf623c3d353dbfc5e
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-06 01:01:56 +0200

    Modify shebang and add copyright notice
    Updated shebang and added license information.

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
 #
 # Custom build script - Xiaomi AX3000T
 # Optimized LuCI + ImmortalWrt enhancements
