a51a67238946cc28c53d0552c6bf79ec308605a7
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-06 00:50:34 +0200

    Fix emortal package path in diy-part2.sh

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -11,7 +11,7 @@
 
 git clone -b openwrt-25.12 https://github.com/immortalwrt/immortalwrt.git tmp_imm
 
-cp -r tmp_imm/package/emortal package/
+cp -r tmp_imm/package/emortal package/emortal
 rm -rf tmp_imm
 
 sed -i '/AUTOLOAD:=$(call AutoProbe,mt7915e)/a \  MODPARAMS.mt7915e:=wed_enable=Y' package/kernel/mt76/Makefile
