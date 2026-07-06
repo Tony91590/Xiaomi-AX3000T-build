b57377a5bfed2d7125b6eadbdee8c47acfd63ee0
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-06 20:07:39 +0200

    Include temperature in system status fields
    Added temperature information to system status display.

--- Xiaomi-AX3000T/10_system.js
+++ Xiaomi-AX3000T/10_system.js
@@ -77,14 +77,18 @@ return baseclass.extend({
 			_('Kernel Version'),   boardinfo.kernel,
 			_('Local Time'),       datestr,
 			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
-			_('Load Average'),     Array.isArray(systeminfo.load) ? '%.2f, %.2f, %.2f'.format(
-				systeminfo.load[0] / 65535.0,
-				systeminfo.load[1] / 65535.0,
-				systeminfo.load[2] / 65535.0
+ 			_('Load Average'),     Array.isArray(systeminfo.load) ? '%.2f, %.2f, %.2f'.format(
+ 				systeminfo.load[0] / 65535.0,
+ 				systeminfo.load[1] / 65535.0,
+ 				systeminfo.load[2] / 65535.0
 			) : null
-		];
 
-		var table = E('table', { 'class': 'table' });
+		if (tempinfo.tempinfo) {
+			fields.splice(6, 0, _('Temperature'));
+			fields.splice(7, 0, tempinfo.tempinfo);
+		}
+
+ 		var table = E('table', { 'class': 'table' });
 
 		for (var i = 0; i < fields.length; i += 2) {
 			table.appendChild(E('tr', { 'class': 'tr' }, [
