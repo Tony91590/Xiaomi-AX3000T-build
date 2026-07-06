a5ec59181d79ca6ea6cc669f4592592d7999728b
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-06 20:19:00 +0200

    Fix formatting of uptime and load average display

--- Xiaomi-AX3000T/10_system.js
+++ Xiaomi-AX3000T/10_system.js
@@ -77,8 +77,7 @@ return baseclass.extend({
 			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || ''),
 			_('Kernel Version'),   boardinfo.kernel,
 			_('Local Time'),       datestr,
-			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
- 			_('Load Average'),     Array.isArray(systeminfo.load) ? '%.2f, %.2f, %.2f'.format(
+			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null, 			_('Load Average'),     Array.isArray(systeminfo.load) ? '%.2f, %.2f, %.2f'.format(
  				systeminfo.load[0] / 65535.0,
  				systeminfo.load[1] / 65535.0,
  				systeminfo.load[2] / 65535.0
