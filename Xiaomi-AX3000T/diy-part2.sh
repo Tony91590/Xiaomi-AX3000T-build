b498b1b0450d0425bd242f1d696c42077853d812
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-07 12:32:55 +0200

    Add device tree source for Redmi AX6000
    Copy device tree source file for Redmi AX6000.

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -7,6 +7,8 @@
 # 
 # Custom for Xiaomi AX3000T
 
+cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/mt7986a-xiaomi-redmi-router-ax6000.dts target/linux/mediatek/dts/
+
 cat > target/linux/mediatek/filogic/base-files/etc/init.d/bootcount << EOF
 #!/bin/sh /etc/rc.common
 # SPDX-License-Identifier: GPL-2.0-only
