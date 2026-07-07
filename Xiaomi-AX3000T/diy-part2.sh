8ea132894edcaf5eb8bee7b13fe3c56b196c68fc
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-07 12:24:58 +0200

    Create platform.sh for Mediatek upgrade
    Add platform.sh script for upgrade process

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -43,3 +43,7 @@ EOF
 mkdir -p target/linux/mediatek/filogic/base-files/etc/rc.d
 ln -sf ../init.d/bootcount target/linux/mediatek/filogic/base-files/etc/rc.d/S99bootcount
 chmod 0755 target/linux/mediatek/filogic/base-files/etc/init.d/bootcount
+
+cat > target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh << EOF
+
+EOF
