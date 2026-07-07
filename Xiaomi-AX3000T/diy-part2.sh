6d1d965380b746a27284f36a9fd756de26393a17
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-07 08:37:42 +0200

    Clean up diy-part2.sh by removing unused commands
    Removed unnecessary sed commands and grep.

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -6,8 +6,4 @@
 # See /LICENSE for more information.
 # 
 # Custom for Xiaomi AX3000T
-sed -i '/\["FR"\]/s/{ 1, 2 }/{ 1, 1 }/' package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
 sed -i 's/default-settings-chn/default-settings/g' include/target.mk
-#sed -i 's/"turboacc\.config\.fullcone"="2"/"turboacc.config.fullcone"="0"/g' package/mtk/applications/luci-app-turboacc-mtk/root/etc/uci-defaults/turboacc
-cat package/mtk/applications/luci-app-turboacc-mtk/root/etc/uci-defaults/turboacc
-grep -R '\["FR"\]' package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
