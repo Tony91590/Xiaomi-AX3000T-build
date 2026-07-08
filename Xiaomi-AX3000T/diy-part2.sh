99eb5daec3c22c8395d313dd489ff9f8aec8e416
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-09 01:10:07 +0200

    Clean up diy-part2.sh by removing test comments
    Removed commented-out test code from the script.

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -7,11 +7,6 @@
 # 
 # Custom for Xiaomi AX3000T stock
 
-# test
-cp $GITHUB_WORKSPACE/Xiaomi-AX3000T/wireless.js feeds/luci/modules/luci-mod-network/htdocs/luci-static/resources/view/network/ \
-    && echo "✅ testest"
-# test
-
 cp $GITHUB_WORKSPACE/Xiaomi-AX3000T/mt7981b-xiaomi-ax3000t.dts target/linux/mediatek/dts/ \
     && echo "✅ mt7981b-xiaomi-ax3000t.dts copié"
 
