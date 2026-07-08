2374241ab6a233bec4b48430008a0cf8722b9eb8
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-08 08:14:54 +0200

    Refactor board case handling in diy-part2.sh

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -123,69 +123,7 @@ platform_check_image() {
 	[ "$#" -gt 1 ] && return 1
 
 	case "$board" in
-	abt,asr3000|\
-	acer,predator-w6x-ubootmod|\
-	asus,zenwifi-bt8-ubootmod|\
-	bananapi,bpi-r3|\
-	bananapi,bpi-r3-mini|\
-	bananapi,bpi-r4|\
-	bananapi,bpi-r4-2g5|\
-	bananapi,bpi-r4-poe|\
-	bananapi,bpi-r4-lite|\
-	bazis,ax3000wm|\
-	cmcc,a10-ubootmod|\
-	cmcc,rax3000m|\
-	comfast,cf-wr632ax-ubootmod|\
-	creatlentem,clt-r30b1-ubi|\
-	cudy,m3000-v1-ubootmod|\
-	cudy,m3000-v2-yt8821-ubootmod|\
-	cudy,tr3000-v1-ubootmod|\
-	cudy,wbr3000uax-v1-ubootmod|\
-	cudy,wr3000e-v1-ubootmod|\
-	cudy,wr3000s-v1-ubootmod|\
-	cudy,wr3000h-v1-ubootmod|\
-	cudy,wr3000p-v1-ubootmod|\
-	gatonetworks,gdsp|\
-	h3c,magic-nx30-pro|\
-	jcg,q30-pro|\
-	jdcloud,re-cp-03|\
-	konka,komi-a31|\
-	mediatek,mt7981-rfb|\
-	mediatek,mt7988a-rfb|\
-	mercusys,mr90x-v1-ubi|\
-	nokia,ea0326gmp|\
-	netis,eap930-v1|\
-	netis,nx32u|\
-	openwrt,one|\
-	netcore,n60|\
-	qihoo,360t7|\
-	qihoo,360t7-ubi|\
-	routerich,ax3000-ubootmod|\
-	tplink,tl-xdr4288|\
-	tplink,tl-xdr6086|\
-	tplink,tl-xdr6088|\
-	tplink,tl-xtr8488|\
-	xiaomi,mi-router-ax3000t-ubootmod|\
-	xiaomi,redmi-router-ax6000-ubootmod|\
-	xiaomi,mi-router-wr30u-ubootmod|\
-	zyxel,ex5601-t0-ubootmod)
-		fit_check_image "$1"
-		return $?
-		;;
-	creatlentem,clt-r30b1|\
-	creatlentem,clt-r30b1-112m|\
-	nradio,c8-668gl)
-		# tar magic `ustar`
-		magic="$(dd if="$1" bs=1 skip=257 count=5 2>/dev/null)"
-
-		[ "$magic" != "ustar" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-
-		return 0
-		;;
-	*)
+	xiaomi,mi-router-ax3000t)
 		nand_do_platform_check "$board" "$1"
 		return $?
 		;;
