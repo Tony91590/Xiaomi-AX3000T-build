0acc1d044c329d2d6593f71547a523cd73cb54c5
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-09 22:55:55 +0200

    Refactor patch application in diy-part2.sh
    Updated script to use a variable for the patch file path and added echo statements for clarity.

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -16,19 +16,21 @@ set -e
 
 #STATUS_FILE="feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js"
 
-echo "[1/5] Downloading LuCI system status patch..."
+#echo "[1/5] Downloading LuCI system status patch..."
 
 #mkdir -p "$(dirname "$STATUS_FILE")"
 
 #curl -fsSL "https://raw.githubusercontent.com/Tony91590/Xiaomi-AX3000T-build/refs/heads/Proto-beta/Xiaomi-AX3000T/10_system.js" \
 #  -o "$STATUS_FILE"
 
-if patch -p1 < patches/10_system.patch; then
-    echo "✓ LuCI system status patch applied successfully."
-else
-    echo "✗ Failed to apply LuCI system status patch."
-    exit 1
-fi
+PATCH_FILE="$GITHUB_WORKSPACE/Xiaomi-AX3000T/10_system.patch"
+
+echo "[1/5] Applying LuCI system status patch..."
+
+patch -p1 < "$PATCH_FILE"
+
+echo "✓ LuCI system status patch applied successfully."
+
 
 echo "[2/5] Patching LuCI RPC backend..."
 
