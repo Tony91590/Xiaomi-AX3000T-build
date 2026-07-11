cd5232e2271aedca0c09ce0b6881534fe2c9aeea
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-11 13:51:30 +0200

    Update comments for Xiaomi AX3000T customization

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -5,7 +5,7 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 # 
-# Custom for Xiaomi AX3000T
+# Custom for Xiaomi AX3000T stock and RD23
 cp $GITHUB_WORKSPACE/Xiaomi-AX3000T/patch/7661-022-fix-rrm-snprintf-error.patch package/mtk/drivers/mt_wifi/patches-7673/
 cp $GITHUB_WORKSPACE/Xiaomi-AX3000T/patch/7661-022-fix-rrm-snprintf-error.patch package/mtk/drivers/mt_wifi/patches-7661/
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
