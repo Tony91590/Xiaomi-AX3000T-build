849308f83767ae264f6ff97f2ea9da5f4ebeddb7
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-16 03:52:49 +0200

    Update diy-part1.sh

--- Xiaomi-AX3000T/diy-part1.sh
+++ Xiaomi-AX3000T/diy-part1.sh
@@ -18,18 +18,3 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
 #
-cat > feeds.conf.default << 'EOF'
-src-git packages https://github.com/coolsnowwolf/packages
-#src-git luci https://github.com/coolsnowwolf/luci
-#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05
-#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-24.10
-src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-25.12
-src-git routing https://github.com/coolsnowwolf/routing
-src-git telephony https://github.com/coolsnowwolf/telephony.git
-#src-git helloworld https://github.com/fw876/helloworld.git
-#src-git qmodem https://github.com/FUjr/modem_feeds.git
-#src-git video https://github.com/openwrt/video.git
-#src-git targets https://github.com/openwrt/targets.git
-#src-git oldpackages http://git.openwrt.org/packages.git
-#src-link custom /usr/src/openwrt/custom-feed
-EOF
