c7ce54baba8262f7a05efd32c8b5be871484c2c2
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-07-25 10:15:52 +0200

    Remove obsolete original files in diy-part2.sh

--- Xiaomi-AX3000T/diy-part2.sh
+++ Xiaomi-AX3000T/diy-part2.sh
@@ -170,5 +170,9 @@ patch -p1 < "$PATCH_DTS"
 chmod 0644 target/linux/mediatek/dts/mt7981b-xiaomi-mi-router-ax3000t-112m-nmbm.dts 
 
 rm -f feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js.orig
+rm -f target/linux/mediatek/base-files/lib/preinit/05_set_preinit_iface.orig
+rm -f target/linux/mediatek/filogic/base-files/etc/board.d/02_network.orig
+rm -f target/linux/mediatek/image/filogic.mk.orig
+rm -f package/boot/uboot-tools/uboot-envtools/files/mediatek_filogic.orig
 
 echo "Done ✔"
